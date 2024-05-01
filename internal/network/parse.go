e6fa37cde8430e8c7a9d752b43b47b94a7d5f67e
Dan Esparza <danesparza@cagedtornado.com>
2024-05-01 08:29:54 -0400

    Add network cli parsing and unit test

---
+++ internal/network/parse.go
@@ -0,0 +1,31 @@
+package network
+
+import "strings"
+
+func parseCliOutput(src string) []string {
+	var fields []string
+	separator := ':'
+
+	var currentField strings.Builder
+	escaping := false
+
+	for _, char := range src {
+		if char == '\\' && !escaping {
+			escaping = true
+			continue
+		}
+
+		if char == separator && !escaping {
+			fields = append(fields, currentField.String())
+			currentField.Reset()
+		} else {
+			currentField.WriteRune(char)
+			escaping = false
+		}
+	}
+
+	// Append the last field
+	fields = append(fields, currentField.String())
+
+	return fields
+}
---
+++ internal/network/parse_test.go
@@ -0,0 +1,36 @@
+package network
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_parseCliOutput(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		src  string
+		want []string
+	}{
+		{
+			name: "Success - valid fields",
+			src:  `*:66\:22\:32\:1D\:24\:E3:Nahual:153:540 Mbit/s:74:WPA2`,
+			want: []string{
+				"*",
+				"66:22:32:1D:24:E3",
+				"Nahual",
+				"153",
+				"540 Mbit/s",
+				"74",
+				"WPA2",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := parseCliOutput(tt.src)
+			assert.Equal(t, tt.want, parseCliOutput(tt.src), "parseCliOutput() = %v, want %v", got, tt.want)
+		})
+	}
+}
